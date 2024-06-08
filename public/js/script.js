02e0c6e3311fe1f063ae71547d623a633d5e57ed
slaughtercasandra787 <slaughtercasandra787@gmail.com>
2024-06-08 17:19:52 -0500

    made the http routes actually work and added validator and middleware file

--- public/js/script.js
+++ public/js/script.js
@@ -1,32 +1,59 @@
-const homeBtn = document.getElementById('submit');
+// const profileBtn = document.getElementById('submit');
 
-  async function handleSubmit(event) {
-    event.preventDefault(); 
+//   async function handleSubmit(event) {
+//     event.preventDefault(); 
 
-    const formData = {
-        Name: document.getElementById('name').value,
-        Passowrd: document.getElementById('pass').value,
-        
-    };
+//     const formData = {
+//         Name: document.getElementById('name').value,
+//         Passowrd: document.getElementById('pass').value,
+//     };
 
-    try {
-        const response = await fetch('/submit', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formData)
-        });
+//     try {
+//         const response = await fetch('/submit', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(formData)
+//         });
 
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
+//         if (!response.ok) {
+//             throw new Error('Network response was not ok');
+//         }
 
-        const data = await response.json();
-        window.location.href = '../../views/layouts/social.handlebars';
+//         const formData = await response.json();
+//         window.location.href = '../../views/layouts/social.handlebars';
  
-    } catch (err) {
-        console.error(err);
-    }
-}
+//     } catch (err) {
+//         console.error(err);
+// };
 
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.getElementById('#name').value.trim();
+    const password = document.qgetElementById('#pass').value.trim();
+  
+    if (username && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to sign up.');
+      }
+    }
+  };
+  
+//   document
+//     .querySelector('.login-form')
+//     .addEventListener('submit', loginFormHandler);
+  
+//   document
+//     .querySelector('.signup-form')
+//     .addEventListener('submit', signupFormHandler);
+  
