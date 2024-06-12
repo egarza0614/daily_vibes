60db68f11cb52cc71238055fb57ea1e8b50da42f
Cadence Higgins <cadencehiggins@gmail.com>
2024-06-12 12:05:44 -0500

    restructured api files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,19 @@
-const router = require('express').Router()
-const userRoutes = require('./api/user')
-const postRoutes = require('./api/post')
-const commentRoutes = require('./api/comment')
-const authentication = require('./api/auth')
+const express = require('express');
+const router = express.Router();
 
+router.get('/signup', function (req, res, next) {
+    res.render('signup.handlebars', { name: 'dailyvibes', email: 'cass@gmail.com' });
+});
 
+router.get('/', function (req, res, next) {
+    res.render('home.handlebars', { title: 'hello' });
+});
 
-router.use('/api/users', userRoutes);
-router.use('/api/posts', postRoutes);
-router.use('/api/comments', commentRoutes);
-router.use('/api/authentication', authentication);
+router.get('/profile', function (req, res, next) {
+    res.render('profile.handlebars', { title: 'dailyvibes' });
+});
 
-
-module.exports = router
+router.get('/posts', function (req, res, next) {
+    res.render('posts.handlebars', { title: 'Your Feed' })
+})
+module.exports = router;
